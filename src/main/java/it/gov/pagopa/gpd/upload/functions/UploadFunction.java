adc19b0498c2e4bb9a66cd8430ff8277e24dbc4d
AngeloCaporaso <angelo.caporaso@pagopa.it>
2024-01-09 17:46:12 +0100

    [PAGOPA-1362] feat: Add validation step and output

--- src/main/java/it/gov/pagopa/gpd/upload/functions/UploadFunction.java
+++ src/main/java/it/gov/pagopa/gpd/upload/functions/UploadFunction.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.OutputBinding;
+
 import com.microsoft.azure.functions.annotation.*;
 import it.gov.pagopa.gpd.upload.entity.FailedIUPD;
 import it.gov.pagopa.gpd.upload.model.ResponseGPD;
@@ -11,14 +13,18 @@ import it.gov.pagopa.gpd.upload.entity.Status;
 import it.gov.pagopa.gpd.upload.entity.Upload;
 import it.gov.pagopa.gpd.upload.exception.AppException;
 import it.gov.pagopa.gpd.upload.model.RetryStep;
+import it.gov.pagopa.gpd.upload.model.pd.PaymentPositionModel;
 import it.gov.pagopa.gpd.upload.model.pd.PaymentPositionsModel;
 import it.gov.pagopa.gpd.upload.client.GpdClient;
 import it.gov.pagopa.gpd.upload.repository.StatusRepository;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -31,6 +37,7 @@ public class UploadFunction {
     private final String BLOCK_SIZE = System.getenv("BLOCK_SIZE");
 
     private final int MESSAGE_MAX_CHAR_NUMBER = 150;
+    private final int BAD_REQUEST = 400;
 
     /**
      * This function will be invoked when a new or updated blob is detected at the
@@ -44,6 +51,11 @@ public class UploadFunction {
                     connection = "GPD_SA_CONNECTION_STRING") byte[] content,
             @BindingName("fiscalCode") String fiscalCode,
             @BindingName("name") String filename,
+            @BlobOutput(
+                    name = "target",
+                    path = "gpd-upload/output/{fiscalCode}/result_{name}",
+                    connection = "GPD_SA_CONNECTION_STRING")
+            OutputBinding<String> outputBlob,
             final ExecutionContext context
     ) {
         Logger logger = context.getLogger();
@@ -53,47 +65,40 @@ public class UploadFunction {
                                              + ", size : " + content.length + " bytes");
 
         String converted = new String(content, StandardCharsets.UTF_8);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        String key = filename.substring(0, filename.indexOf("."));
 
         try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.registerModule(new JavaTimeModule());
-            PaymentPositionsModel paymentPositionsModel = objectMapper.readValue(converted, PaymentPositionsModel.class);
-            logger.log(Level.INFO, () -> "Payment positions size: " + paymentPositionsModel.getPaymentPositions().size());
-            createPaymentPositionBlocks(logger, context.getInvocationId(), fiscalCode, filename.substring(0, filename.indexOf(".")), paymentPositionsModel);
+            // deserialize payment positions from JSON to Object
+            PaymentPositionsModel pps = objectMapper.readValue(converted, PaymentPositionsModel.class);
+            Status status = this.createStatus(logger, fiscalCode, key, pps);
+            logger.log(Level.INFO, () -> "Payment positions size: " + pps.getPaymentPositions().size());
+            // function logic: validation and block upload to GPD-Core
+            validate(logger, pps, status);
+            status = createPaymentPositionBlocks(logger, context.getInvocationId(), fiscalCode, key, pps, status);
+            // write status in output container
+            outputBlob.setValue(objectMapper.writeValueAsString(status));
         } catch (JsonProcessingException | AppException e) {
             logger.log(Level.INFO, () -> "Processing blob exception: " + e.getMessage());
         } catch (Exception e) {
-            logger.log(Level.INFO, () -> "Processing blob exception: " + e.getMessage());
+            logger.log(Level.INFO, () -> "Processing blob exception: " + e);
         }
     }
 
 
-    public void createPaymentPositionBlocks(Logger logger, String invocationId, String fiscalCode, String key, PaymentPositionsModel paymentPositionsModel) throws Exception {
+    public Status createPaymentPositionBlocks(Logger logger, String invocationId, String fc, String key, PaymentPositionsModel pps, Status status) throws Exception {
         long t1 = System.currentTimeMillis();
-        Status statusIfNotExist = Status.builder()
-                                    .id(key)
-                                    .fiscalCode(fiscalCode)
-                                    .upload(Upload.builder()
-                                                    .current(0)
-                                                    .total(paymentPositionsModel.getPaymentPositions().size())
-                                                    .successIUPD(new ArrayList<>())
-                                                    .failedIUPDs(new ArrayList<>())
-                                                    .start(LocalDateTime.now()).build())
-                                    .build();
-        Status status = StatusRepository.getInstance(logger).createIfNotExist(key, fiscalCode, statusIfNotExist);
-        if(status.upload.getEnd() != null) {
-            logger.log(Level.INFO, () -> "Upload already processed. Upload finished at " + status.upload.getEnd());
-            return;
-        }
+
         int blockSize = Integer.parseInt(BLOCK_SIZE);
         int index = 0;
-        int totalPosition = paymentPositionsModel.getPaymentPositions().size();
+        int totalPosition = pps.getPaymentPositions().size();
         PaymentPositionsModel block;
         while(index + blockSize < totalPosition) {
             logger.log(Level.INFO,
                     "Process block for payment positions from index " + index + ", block size: " + blockSize + ", total size: " + totalPosition);
-            block = new PaymentPositionsModel(paymentPositionsModel.getPaymentPositions().subList(index, index+blockSize));
-            ResponseGPD response = GpdClient.getInstance().createDebtPositions(fiscalCode, block, logger, invocationId);
+            block = new PaymentPositionsModel(pps.getPaymentPositions().subList(index, index+blockSize));
+            ResponseGPD response = GpdClient.getInstance().createDebtPositions(fc, block, logger, invocationId);
             List<String> IUPDs = block.getPaymentPositions().stream().map(item -> item.getIupd()).collect(Collectors.toList());
             this.updateStatus(IUPDs, status, response, blockSize);
             StatusRepository.getInstance(logger).upsertStatus(key, status);
@@ -104,8 +109,8 @@ public class UploadFunction {
         if(remainingPosition > 0) {
             logger.log(Level.INFO,
                     "Process last block for payment positions from index " + index + ", remaining position: " + remainingPosition + ", total size: " + totalPosition);
-            block = new PaymentPositionsModel(paymentPositionsModel.getPaymentPositions().subList(index, index+remainingPosition));
-            ResponseGPD response = GpdClient.getInstance().createDebtPositions(fiscalCode, block, logger, invocationId);
+            block = new PaymentPositionsModel(pps.getPaymentPositions().subList(index, index+remainingPosition));
+            ResponseGPD response = GpdClient.getInstance().createDebtPositions(fc, block, logger, invocationId);
             List<String> IUPDs = block.getPaymentPositions().stream().map(pp -> pp.getIupd()).collect(Collectors.toList());
             this.updateStatus(IUPDs, status, response, remainingPosition);
             StatusRepository.getInstance(logger).upsertStatus(key, status);
@@ -117,6 +122,64 @@ public class UploadFunction {
 
         long uploadDuration = System.currentTimeMillis() - t1;
         logger.log(Level.INFO, "Elapsed upload blocks time: " + uploadDuration);
+
+        return status;
+    }
+
+    private void validate(Logger logger, PaymentPositionsModel paymentPositionsModel, Status status) {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<PaymentPositionModel>> violations;
+        ArrayList<FailedIUPD> failedIUPDs = status.upload.getFailedIUPDs();
+
+        List<String> skippedIUPDs;
+        Iterator<PaymentPositionModel> iterator = paymentPositionsModel.getPaymentPositions().iterator();
+        while (iterator.hasNext()) {
+            PaymentPositionModel pp = iterator.next();
+            violations =  validator.validate(pp);
+
+            if (!violations.isEmpty()) {
+                skippedIUPDs = new ArrayList<>();
+                skippedIUPDs.add(pp.getIupd());
+                ConstraintViolation<PaymentPositionModel> violation = violations.stream().findFirst().orElse(null);
+                String details = (violation != null ? violation.getMessage() : "");
+                FailedIUPD failedIUPD = FailedIUPD.builder()
+                                                .errorCode(BAD_REQUEST)
+                                                .details("BAD REQUEST " + details)
+                                                .skippedIUPDs(skippedIUPDs).build();
+                failedIUPDs.add(failedIUPD);
+                iterator.remove();
+
+                for(ConstraintViolation<PaymentPositionModel> v : violations) {
+                    logger.log(Level.INFO, "Payment position " + pp.getIupd() + " is not valid, violation: " + v.getMessage());
+                }
+            } else {
+                logger.log(Level.INFO, "Payment position list is valid");
+            }
+        }
+
+        status.upload.setCurrent(status.upload.getCurrent() + failedIUPDs.size());
+        status.upload.setFailedIUPDs(failedIUPDs);
+    }
+
+    private Status createStatus(Logger logger, String fiscalCode, String key, PaymentPositionsModel paymentPositionsModel) throws AppException {
+        Status statusIfNotExist = Status.builder()
+                                          .id(key)
+                                          .fiscalCode(fiscalCode)
+                                          .upload(Upload.builder()
+                                                          .current(0)
+                                                          .total(paymentPositionsModel.getPaymentPositions().size())
+                                                          .successIUPD(new ArrayList<>())
+                                                          .failedIUPDs(new ArrayList<>())
+                                                          .start(LocalDateTime.now()).build())
+                                          .build();
+        Status status = StatusRepository.getInstance(logger).createIfNotExist(key, fiscalCode, statusIfNotExist);
+        if(status.upload.getEnd() != null) {
+            logger.log(Level.INFO, () -> "Upload already processed. Upload finished at " + status.upload.getEnd());
+            return status;
+        }
+
+        return status;
     }
 
     public Status updateStatus(List<String> IUPDs, Status status, ResponseGPD response, int blockSize) {
@@ -134,6 +197,7 @@ public class UploadFunction {
             status.upload.addFailures(failedIUPD);
         }
         status.upload.setCurrent(status.upload.getCurrent() + blockSize);
+
         return status;
     }
 }
--- src/main/java/it/gov/pagopa/gpd/upload/model/pd/PaymentOptionModel.java
+++ src/main/java/it/gov/pagopa/gpd/upload/model/pd/PaymentOptionModel.java
@@ -9,10 +9,10 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
