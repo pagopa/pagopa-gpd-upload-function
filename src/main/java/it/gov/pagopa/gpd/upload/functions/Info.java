9729482b13fbcf1fa14fad3a188c7ace5ea209f8
AngeloCaporaso <angelo.caporaso@pagopa.it>
2024-01-10 12:45:53 +0100

    [PAGOPA-1362] ci: Update port

--- src/main/java/it/gov/pagopa/gpd/upload/functions/Info.java
+++ src/main/java/it/gov/pagopa/gpd/upload/functions/Info.java
@@ -1,6 +1,10 @@
 package it.gov.pagopa.gpd.upload.functions;
 
-import com.microsoft.azure.functions.*;
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
