41fdfc6dfda1bde2d60aacb154095d3908b690cb
AngeloCaporaso <angelo.caporaso@pagopa.it>
2024-01-10 19:05:44 +0100

    [PAGOPA-1362] fix

--- src/main/java/it/gov/pagopa/gpd/upload/Info.java
+++ src/main/java/it/gov/pagopa/gpd/upload/Info.java
@@ -10,34 +10,30 @@ import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
 import it.gov.pagopa.gpd.upload.model.AppInfo;
 
-
 import java.io.InputStream;
 import java.util.Optional;
 import java.util.Properties;
 import java.util.logging.Logger;
 
-
-/**
- * Azure Functions with Azure Http trigger.
- */
+/** Azure Functions with Azure Http trigger. */
 public class Info {
 
 	/**
 	 * This function will be invoked when a Http Trigger occurs
+	 *
 	 * @return
 	 */
 	@FunctionName("Info")
-	public HttpResponseMessage run (
-			@HttpTrigger(name = "InfoTrigger",
-			methods = {HttpMethod.GET},
-			route = "info",
-			authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
+	public HttpResponseMessage run(
+			@HttpTrigger(
+					name = "InfoTrigger",
+					methods = {HttpMethod.GET},
+					route = "info",
+					authLevel = AuthorizationLevel.ANONYMOUS)
+			HttpRequestMessage<Optional<String>> request,
 			final ExecutionContext context) {
 
-		return request.createResponseBuilder(HttpStatus.OK)
-				.header("Content-Type", "application/json")
-				.body(getInfo(context.getLogger(), "/maven-archiver/pom.properties"))
-				.build();
+		return request.createResponseBuilder(HttpStatus.OK).build();
 	}
 
 	public synchronized AppInfo getInfo(Logger logger, String path) {
