d9326c1339971149eee0fd0c184535784a4aea41
AngeloCaporaso <angelo.caporaso@pagopa.it>
2024-01-27 18:42:56 +0100

    [PAGOPA-1446] fix

--- src/main/java/it/gov/pagopa/gpd/upload/BlobEventFunction.java
+++ src/main/java/it/gov/pagopa/gpd/upload/BlobEventFunction.java
@@ -1,12 +1,16 @@
 package it.gov.pagopa.gpd.upload;
 
-import com.azure.core.util.BinaryData;
+import com.azure.messaging.eventgrid.EventGridEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microsoft.azure.functions.*;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -21,54 +25,36 @@ public class BlobEventFunction {
                     methods = {HttpMethod.POST, HttpMethod.GET, HttpMethod.PUT},
                     route = "upload",
                     authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<String> request,
-            final ExecutionContext context) {
+            final ExecutionContext context) throws JsonProcessingException {
         Logger logger = context.getLogger();
 
         logger.log(Level.INFO, () -> "Request body: " + request.getBody());
-        BinaryData events = BinaryData.fromString(String.valueOf(request.getBody()));
-        logger.log(Level.INFO, () -> "Request body: " + events.toString());
-        Map<String, Object> eventsDictionary = convertStringToDictionary(String.valueOf(events));
-        logger.log(Level.INFO, () -> "Events: " + eventsDictionary);
+        List<EventGridEvent> eventGridEvents = EventGridEvent.fromString(request.getBody());
 
-        HttpResponseMessage httpResponseMessage;
-        if(eventsDictionary.get("eventType").equals("Microsoft.EventGrid.SubscriptionValidationEvent")) {
-            httpResponseMessage =  request.createResponseBuilder(HttpStatus.OK)
-                                                               .body("{\"validationResponse\":"+eventsDictionary.get("validationCode") + "}")
-                                                               .build();
-            logger.log(Level.INFO, () -> "Response: " + httpResponseMessage.getBody());
-        } else {
-            httpResponseMessage =  request.createResponseBuilder(HttpStatus.OK)
-                                                               .build();
+        if(eventGridEvents == null) {
+            return request.createResponseBuilder(HttpStatus.NOT_FOUND).build();
         }
 
-        return httpResponseMessage;
-    }
+        for (EventGridEvent event : eventGridEvents) {
+            if(event.getEventType().equals("Microsoft.EventGrid.SubscriptionValidationEvent")) {
+                logger.log(Level.INFO, () -> "Microsoft.EventGrid.SubscriptionValidationEvent: " + event);
+                JsonNode jsonNode = new ObjectMapper().readTree(event.getData().toString());
+                JsonNode dataNode = jsonNode.path("data");
+                String validationCode = dataNode.path("validationCode").asText();
+                String validationUrl = dataNode.path("validationUrl").asText();
+                logger.log(Level.INFO, () -> "validationCode: " + validationCode);
+                logger.log(Level.INFO, () -> "validationUrl: " + validationUrl);
 
-    private static Map<String, Object> convertStringToDictionary(String inputString) {
-        Map<String, Object> dictionary = new HashMap<>();
-
-        // Extract key-value pairs using regex
-        Pattern pattern = Pattern.compile("(\\w+)=([^,}]+)");
-        Matcher matcher = pattern.matcher(inputString);
-
-        while (matcher.find()) {
-            String key = matcher.group(1);
-            String value = matcher.group(2).trim();
-
-            // If the value is an object, convert it to a nested dictionary
-            if (value.startsWith("{") && value.endsWith("}")) {
-                value = value.substring(1, value.length() - 1);
-                Map<String, Object> nestedDictionary = convertStringToDictionary(value);
-                dictionary.put(key, nestedDictionary);
-            } else if (key.equals("data")) {
-                // Handle special case for the 'data' field
-                Map<String, Object> nestedData = convertStringToDictionary(value);
-                dictionary.putAll(nestedData);
-            } else {
-                dictionary.put(key, value);
+                return request.createResponseBuilder(HttpStatus.OK)
+                                               .body("{\"validationResponse\":"+validationCode + "}")
+                                               .build();
+            } else if(event.getEventType().equals("Microsoft.Storage.BlobCreated")){
+                logger.log(Level.INFO, () -> "Microsoft.Storage.BlobCreated: " + event);
+                return request.createResponseBuilder(HttpStatus.OK)
+                               .build();
             }
         }
 
-        return dictionary;
+        return request.createResponseBuilder(HttpStatus.OK).build();
     }
 }
