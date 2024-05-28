98cece783de443ad88104f4b7ef3d39caefa13d4
AngeloCaporaso <angelo.caporaso@pagopa.it>
2024-05-28 19:19:35 +0200

    refactoring CRUDService logging

--- src/main/java/it/gov/pagopa/gpd/upload/service/CRUDService.java
+++ src/main/java/it/gov/pagopa/gpd/upload/service/CRUDService.java
@@ -22,6 +22,7 @@ public class CRUDService {
             System.getenv("MAX_RETRY") != null ? Integer.parseInt(System.getenv("MAX_RETRY")) : 2;
     private static final Integer RETRY_DELAY =
             System.getenv("RETRY_DELAY_IN_SECONDS") != null ? Integer.parseInt(System.getenv("RETRY_DELAY_IN_SECONDS")) : 300;
+    private static final String LOG_ID = "[id=%s][upload-key=%s][OperationService] ";
 
     private final ObjectMapper om;
     private final DebtPositionMessage debtPositionMessage;
@@ -39,13 +40,13 @@ public class CRUDService {
     }
 
     private ResponseGPD applyRequest(RequestGPD requestGPD) {
-        ctx.getLogger().log(Level.INFO, () -> String.format("[id=%s][OperationService] Call GPD-Client", ctx.getInvocationId()));
+        ctx.getLogger().log(Level.OFF, () -> String.format(LOG_ID + "Call GPD-Client", ctx.getInvocationId(), debtPositionMessage.getUploadKey()));
         return method.apply(requestGPD);
     }
 
     // constraint: paymentPositions size less than max bulk item per call -> compliant by design(max queue message = 64KB = ~30 PaymentPosition)
     public void processRequestInBulk() throws AppException, JsonProcessingException {
-        ctx.getLogger().log(Level.INFO, () -> String.format("[id=%s][OperationService] Process request in BULK", ctx.getInvocationId()));
+        ctx.getLogger().log(Level.INFO, () -> String.format(LOG_ID + "Process request in BULK", ctx.getInvocationId(), debtPositionMessage.getUploadKey()));
 
         RequestGPD requestGPD = debtPositionMessage.getRequest(RequestTranslator.getInstance(), RequestGPD.Mode.BULK, Optional.empty());
         List<String> IUPDList = debtPositionMessage.getIUPDList();
@@ -54,7 +55,9 @@ public class CRUDService {
         if(!response.is2xxSuccessful()) {
             // if BULK creation wasn't successful, switch to single debt position creation
             Map<String, ResponseGPD> responseByIUPD = processRequestOneByOne(IUPDList);
-            ctx.getLogger().log(Level.INFO, () -> String.format("[id=%s][OperationService] Call Status update for %s IUPDs", ctx.getInvocationId(), responseByIUPD.keySet().size()));
+
+            ctx.getLogger().log(Level.INFO, () -> String.format(LOG_ID + "Call Status update for %s IUPDs",
+                    ctx.getInvocationId(), debtPositionMessage.getUploadKey(), responseByIUPD.keySet().size()));
             statusService.appendResponses(ctx.getInvocationId(), debtPositionMessage.getOrganizationFiscalCode(), debtPositionMessage.getUploadKey(), responseByIUPD);
         } else {
             // if BULK creation was successful
@@ -63,7 +66,7 @@ public class CRUDService {
     }
 
     private Map<String, ResponseGPD> processRequestOneByOne(List<String> IUPDList) throws JsonProcessingException {
-        ctx.getLogger().log(Level.INFO, () -> String.format("[id=%s][OperationService] Single mode processing", ctx.getInvocationId()));
+        ctx.getLogger().log(Level.INFO, () -> String.format(LOG_ID + "Single mode processing", ctx.getInvocationId(), debtPositionMessage.getUploadKey()));
         Map<String, ResponseGPD> responseByIUPD = new HashMap<>();
 
         for(String IUPD: IUPDList) {
@@ -90,7 +93,8 @@ public class CRUDService {
         debtPositionMessage.setRetryCounter(debtPositionMessage.getRetryCounter()+1);
         List<String> retryIUPD = retryResponse.keySet().stream().toList();
         QueueMessage queueMessage = debtPositionMessage.getQueueMessage(MessageTranslator.getInstance(), retryIUPD);
-        ctx.getLogger().log(Level.INFO, () -> String.format("[id=%s][OperationService] Retry message %s", ctx.getInvocationId(), queueMessage.getUploadKey()));
+        ctx.getLogger().log(Level.INFO, () -> String.format(LOG_ID + "Retry message %s",
+                ctx.getInvocationId(), debtPositionMessage.getUploadKey(), queueMessage.getUploadKey()));
         return QueueService.getInstance(ctx.getLogger()).enqueue(ctx.getInvocationId(), om.writeValueAsString(queueMessage), RETRY_DELAY);
     }
 }
